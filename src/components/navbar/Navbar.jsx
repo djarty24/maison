4e37b9b136960c41fcd750d9ea9bd33db07eb9d7
Revati Tambe <24revati@gmail.com>
2024-03-29 23:03:28 -0700

    almost done w/registration

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [isOpen, setOpen] = useState(false)
 
   return (
-    <nav className='flex items-center justify-between p-4 md:py-6 md:px-10 text-xl' style={{fontFamily: "'Raleway', sans-serif"}}>
+    <nav className='flex items-center justify-between mb-12 p-4 md:py-6 md:px-10 text-xl' style={{fontFamily: "'Raleway', sans-serif"}}>
         <Link to={"/"} className='flex flex-row items-center gap-5 text-lg'>
                 <img className='aspect-square w-10' src="/favicon.png" alt="maison-logo" />
                 <h2 className='text-2xl'>Maison</h2>
